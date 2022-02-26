d6fb6a919bd498424d858daa8d3b9bf9e029bf39
Gabriel Nascimento <gabrielnascimento_17@hotmail.com>
2022-02-26 02:48:38 -0300

    fix error of the mvn:compile

--- src/main/java/br/org/recode/CrudApplication.java
+++ src/main/java/br/org/recode/CrudApplication.java
@@ -3,7 +3,9 @@ package br.org.recode;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.stereotype.Component;
 
+@Component
 @SpringBootApplication (exclude = {SecurityAutoConfiguration.class })
 public class CrudApplication {
 
